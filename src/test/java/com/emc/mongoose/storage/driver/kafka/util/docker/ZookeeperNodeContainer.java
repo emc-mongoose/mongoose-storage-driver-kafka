01b3ce0a4f1255cdd00a9f238401e57e115f06ed
Pavel Vinogradov <vin.pavel13@gmail.com>
2019-03-17 22:19:33 +0300

    Correct pre-commit usage
    “Signed-off-by: [Pavel Vinogradov] [vin.pavel13@gmail.com]”
    
    Developer's Certificate of Origin 1.1
    
    By making a contribution to this project, I certify that:
    
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it.
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.

--- src/test/java/com/emc/mongoose/storage/driver/kafka/util/docker/ZookeeperNodeContainer.java
+++ src/test/java/com/emc/mongoose/storage/driver/kafka/util/docker/ZookeeperNodeContainer.java
@@ -6,61 +6,53 @@ import com.github.dockerjava.api.command.CreateNetworkResponse;
 import com.github.dockerjava.api.exception.NotFoundException;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.command.PullImageResultCallback;
-
 import java.io.Closeable;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-public class ZookeeperNodeContainer
-implements Closeable {
+public class ZookeeperNodeContainer implements Closeable {
 
-        private static final Logger LOG = Logger.getLogger(ZookeeperNodeContainer.class.getSimpleName());
-        private static final String IMAGE_NAME = "zookeeper:3.4";
-        private static final DockerClient DOCKER_CLIENT = DockerClientBuilder.getInstance().build();
-        private static String NETWORK_ID = null;
-        private static String CONTAINER_ID = null;
+  private static final Logger LOG = Logger.getLogger(ZookeeperNodeContainer.class.getSimpleName());
+  private static final String IMAGE_NAME = "zookeeper:3.4";
+  private static final DockerClient DOCKER_CLIENT = DockerClientBuilder.getInstance().build();
+  private static String NETWORK_ID = null;
+  private static String CONTAINER_ID = null;
 
-	public ZookeeperNodeContainer()
-	throws Exception {
-            try {
-
-                DOCKER_CLIENT.inspectImageCmd(IMAGE_NAME).exec();
-            } catch (final NotFoundException e) {
-                DOCKER_CLIENT
-                        .pullImageCmd(IMAGE_NAME)
-                        .exec(new PullImageResultCallback())
-                        .awaitCompletion();
-            }
-
-            final CreateNetworkResponse network = DOCKER_CLIENT
-                .createNetworkCmd()
-                .withName("kafka-net").exec();
-            NETWORK_ID = network.getId();
-            LOG.info("docker network create " + NETWORK_ID + "...");
-
-            final CreateContainerResponse container = DOCKER_CLIENT
-                    .createContainerCmd(IMAGE_NAME)
-                    .withName("zookeeper")
-                    .withNetworkMode("kafka-net")//--network kafka-net
-                    .withAttachStderr(true)
-                    .withAttachStdout(true)
-                    .exec();
-            CONTAINER_ID = container.getId();
-            LOG.info("docker start " + CONTAINER_ID + "...");
-            DOCKER_CLIENT.startContainerCmd(CONTAINER_ID).exec();
-        }
-
-        public final void close() {
-            if (CONTAINER_ID != null) {
-                LOG.info("docker kill " + CONTAINER_ID + "...");
-                DOCKER_CLIENT.killContainerCmd(CONTAINER_ID).exec();
-                LOG.info("docker rm " + CONTAINER_ID + "...");
-                DOCKER_CLIENT.removeContainerCmd(CONTAINER_ID).exec();
-                CONTAINER_ID = null;
-            }
-            if (NETWORK_ID != null) {
-                LOG.info("docker network rm " + NETWORK_ID + "...");
-                DOCKER_CLIENT.removeNetworkCmd(NETWORK_ID).exec();
-            }
-        }
+  public ZookeeperNodeContainer() throws Exception {
+    try {
+      DOCKER_CLIENT.inspectImageCmd(IMAGE_NAME).exec();
+    } catch (final NotFoundException e) {
+      DOCKER_CLIENT.pullImageCmd(IMAGE_NAME).exec(new PullImageResultCallback()).awaitCompletion();
     }
+
+    final CreateNetworkResponse network =
+        DOCKER_CLIENT.createNetworkCmd().withName("kafka-net").exec();
+    NETWORK_ID = network.getId();
+    LOG.info("docker network create " + NETWORK_ID + "...");
+
+    final CreateContainerResponse container =
+        DOCKER_CLIENT
+            .createContainerCmd(IMAGE_NAME)
+            .withName("zookeeper")
+            .withNetworkMode("kafka-net") // --network kafka-net
+            .withAttachStderr(true)
+            .withAttachStdout(true)
+            .exec();
+    CONTAINER_ID = container.getId();
+    LOG.info("docker start " + CONTAINER_ID + "...");
+    DOCKER_CLIENT.startContainerCmd(CONTAINER_ID).exec();
+  }
+
+  public final void close() {
+    if (CONTAINER_ID != null) {
+      LOG.info("docker kill " + CONTAINER_ID + "...");
+      DOCKER_CLIENT.killContainerCmd(CONTAINER_ID).exec();
+      LOG.info("docker rm " + CONTAINER_ID + "...");
+      DOCKER_CLIENT.removeContainerCmd(CONTAINER_ID).exec();
+      CONTAINER_ID = null;
+    }
+    if (NETWORK_ID != null) {
+      LOG.info("docker network rm " + NETWORK_ID + "...");
+      DOCKER_CLIENT.removeNetworkCmd(NETWORK_ID).exec();
+    }
+  }
+}
