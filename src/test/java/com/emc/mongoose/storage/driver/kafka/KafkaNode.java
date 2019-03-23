6fbfe75a54ac4d9fec65dbc9004f2d1d04cb2d66
Andrey Kurilov <andrey.kurilov@emc.com>
2019-03-24 01:48:05 +0300

    retry

--- src/test/java/com/emc/mongoose/storage/driver/kafka/KafkaNode.java
+++ src/test/java/com/emc/mongoose/storage/driver/kafka/KafkaNode.java
@@ -5,11 +5,11 @@ public interface KafkaNode {
   int PORT = 9092;
 
   static String addr() {
-    /*final boolean ciFlag = null != System.getenv("CI");
-    if(ciFlag) {
-    	return System.getenv("SERVICE_HOST");
-    } else {*/
-    return "localhost";
-    // }
+    final boolean ciFlag = null != System.getenv("CI");
+    if (ciFlag) {
+      return System.getenv("SERVICE_HOST");
+    } else {
+      return "localhost";
+    }
   }
 }
