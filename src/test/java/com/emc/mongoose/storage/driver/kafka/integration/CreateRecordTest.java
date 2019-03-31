f13bad60d1478559933579f9e70bf7ae03996052
Anastasia Shemyakinskaya <shem98a@mail.ru>
2019-03-31 14:02:01 +0300

    Added exception check to avoid nullpointerexception Developer's Certificate of Origin 1.1
    By making a contribution to this project, I certify that:
    
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it.
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.
    
    Signed-off-by: Anastasia Shemyakinskaya shem98a@gmail.com

--- src/test/java/com/emc/mongoose/storage/driver/kafka/integration/CreateRecordTest.java
+++ src/test/java/com/emc/mongoose/storage/driver/kafka/integration/CreateRecordTest.java
@@ -33,28 +33,30 @@ public class CreateRecordTest {
     properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, host_port);
     Properties producer_properties = new Properties();
     producer_properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host_port);
-    producer_properties.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, 1050000);
-
     kafkaProducer =
-        new KafkaProducer<>(producer_properties, new StringSerializer(), new ByteArraySerializer());
+      new KafkaProducer<>(producer_properties, new StringSerializer(), new ByteArraySerializer());
     adminClient = KafkaAdminClient.create(properties);
   }
 
   @Test
   public void testCreateRecord() throws Exception {
     adminClient.createTopics(Collections.singletonList(new NewTopic(TOPIC_NAME, 1, (short) 1)));
-    final byte[] one_mb_of_data = new byte[1048576];
+    final byte[] one_mb_of_data = new byte[999970];
     final ProducerRecord<String, byte[]> producerRecord =
-        new ProducerRecord<>(TOPIC_NAME, KEY_NAME, one_mb_of_data);
+      new ProducerRecord<>(TOPIC_NAME, KEY_NAME, one_mb_of_data);
     kafkaProducer.send(
-        producerRecord,
-        (metaData, exception) -> {
+      producerRecord,
+      (metaData, exception) -> {
+        if (exception != null){
+          exception.printStackTrace();
+        }else {
           System.out.println("Record was sent");
           Assert.assertEquals("Offset must be 0", 0, metaData.offset());
           Assert.assertEquals(
-              "Name of the topic must be " + TOPIC_NAME, TOPIC_NAME, metaData.topic());
+            "Name of the topic must be " + TOPIC_NAME, TOPIC_NAME, metaData.topic());
           Assert.assertEquals("Value size must be 1 mb", metaData.serializedValueSize(), 1048576);
-        });
+        }
+      });
   }
 
   @After
