9fe25f9f92669e81fc503d45750e45189bde271b
Darya Merkureva <mdp1998@icloud.com>
2019-07-02 08:45:26 -0700

    Make all integration tests the same and remove the containerization in them, fix ReadRecordTest.java Developer's Certificate of Origin 1.1
    By making a contribution to this project, I certify that:
    
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it.
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.
    
    Signed-off-by: Darya Merkureva mdp1998@icloud.com

--- src/test/java/com/emc/mongoose/storage/driver/kafka/integration/ReadRecordTest.java
+++ src/test/java/com/emc/mongoose/storage/driver/kafka/integration/ReadRecordTest.java
@@ -1,79 +1,93 @@
 package com.emc.mongoose.storage.driver.kafka.integration;
 
-import com.emc.mongoose.storage.driver.kafka.util.docker.KafkaNodeContainer;
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.time.Duration;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicLong;
+import lombok.val;
 import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.*;
 
 public class ReadRecordTest {
-  private static KafkaNodeContainer kafkaNodeContainer;
   private Consumer<String, String> consumer;
   private Producer<String, String> producer;
-  private static final String TOPIC_NAME = "topic" + ReadRecordTest.class.getSimpleName();
+  private static AdminClient adm;
+  private static Properties prodProps = new Properties();
+  private static Properties consProps = new Properties();
+  private static Properties admProps = new Properties();
+
+  private static final String TOPIC_NAME =
+      "topic" + ReadRecordTest.class.getSimpleName() + System.currentTimeMillis();
   private static final String KEY_NAME = "key" + ReadRecordTest.class.getSimpleName();
   private static final String DATA = "test-record";
-  private static final Duration TIMEOUT = Duration.ZERO;
-  private static Properties props;
-
-  @BeforeClass
-  public static void initContainer() {
-    try {
-      kafkaNodeContainer = new KafkaNodeContainer();
-    } catch (final Exception e) {
-      throw new AssertionError(e);
-    }
-  }
-
-  @AfterClass
-  public static void closeContainer() {
-    kafkaNodeContainer.close();
-  }
+  private static final String IP = "localhost:9092";
+  private static final Duration TIMEOUT = Duration.ofMillis(1000);
+  private static final Duration TIMEOUT_ZERO = Duration.ZERO;
 
   @Before
-  public void initMockConsumer() {
-    props = new Properties();
-    props.put(
-        AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaNodeContainer.getContainerIp() + ":9092");
-    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+  public void init() {
+    prodProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, IP);
+    prodProps.put(ProducerConfig.CLIENT_ID_CONFIG, "DemoProducer");
+    prodProps.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 50000);
+    prodProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    prodProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    producer = new KafkaProducer<>(prodProps);
 
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    props.put("group.id", "null");
-    consumer = new KafkaConsumer<>(props);
-    producer = new KafkaProducer<>(props);
+    admProps.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, IP);
+    adm = KafkaAdminClient.create(admProps);
+    adm.createTopics(Collections.singletonList(new NewTopic(TOPIC_NAME, 1, (short) 1)));
+
+    consProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, IP);
+    consProps.put(ConsumerConfig.GROUP_ID_CONFIG, "DemoConsumer");
+    consProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+    consProps.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+    consProps.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+    consProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    consProps.put(
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    consumer = new KafkaConsumer<>(consProps);
+  }
+
+  @Test
+  public void readRecordTest() throws Exception {
+    consumer.subscribe(singletonList(TOPIC_NAME));
+    consumer.poll(TIMEOUT).records(TOPIC_NAME);
+    val producerRecord = new ProducerRecord<>(TOPIC_NAME, KEY_NAME, DATA);
+    val offset = new AtomicLong(-1);
+    producer.send(
+        producerRecord,
+        (metadata, exception) -> {
+          assertNull(exception);
+          offset.set(metadata.offset());
+        });
+    producer.flush();
+    assertTrue(offset.get() >= 0);
+    val recordsRead = consumer.poll(TIMEOUT_ZERO).records(TOPIC_NAME);
+    var found = false;
+    for (val rec : recordsRead) {
+      if (rec.offset() == offset.get()) {
+        found = true;
+        break;
+      }
+    }
+    assertTrue(found);
   }
 
   @After
-  public void closeMockConsumer() {
+  public void close() {
     if (consumer != null) {
       consumer.close();
     }
     if (producer != null) {
       producer.close();
-    }
-  }
-
-  @Test
-  public void readRecordTest() throws Exception {
-    consumer.subscribe(Arrays.asList(TOPIC_NAME));
-    final ProducerRecord<String, String> producerRecord =
-        new ProducerRecord<>(TOPIC_NAME, KEY_NAME, DATA);
-    producer.send(producerRecord);
-
-    final ConsumerRecords<String, String> recordsRead = consumer.poll(TIMEOUT);
-    for (ConsumerRecord<String, String> consumerRecordRead : recordsRead) {
-      Assert.assertEquals(
-          "Record value must be " + producerRecord.value(),
-          producerRecord.value(),
-          consumerRecordRead.value());
-      if (recordsRead.iterator().hasNext()) {
-        break;
-      }
     }
   }
 }
